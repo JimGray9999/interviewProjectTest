9cd5a9d34deb407c9d0ab19e3812e14e56c0b6a6
Jim Gray <jgray8689@gmail.com>
2024-01-28 12:41:56 -0500

    added ExtentReport

--- src/test/java/api/testInventory.java
+++ src/test/java/api/testInventory.java
@@ -1,5 +1,8 @@
 package api;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import models.Inventory;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -12,6 +15,9 @@ import java.util.ArrayList;
  */
 public class testInventory {
     ArrayList<Inventory> inventoryList;
+    ExtentReports extent = new ExtentReports();
+    ExtentSparkReporter spark = new ExtentSparkReporter("target/TestReport_012824.html");
+
     @BeforeTest
     public void testSetup() {
         inventoryList = new ArrayList<>();
@@ -20,38 +26,51 @@ public class testInventory {
 
 
     /**
-     * Test for successfully adding a new item to the inventoryList
+     * Test to validate adding a new item to the inventoryList
      * @author: jimgray9999
      */
     @Test
     public void putAddItem() {
-        try {
-            inventoryList.add(new Inventory("Socks", 9.95, 600));
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
+        inventoryList.add(new Inventory("Socks", 9.95, 600));
         printAllItems(inventoryList);
+        extent.attachReporter(spark);
+        extent.createTest("putAddItem")
+                .log(Status.PASS, "Item added successfully");
+        extent.flush();
     }
 
 
     // TODO remove item
     /**
-     * Test for successfully removing an item to the inventoryList
+     * Test to validate removing an item from the inventoryList
      * @author: jimgray9999
      */
     @Test
     public void removeItem() {
-        System.out.println(inventoryList.toString());
+        extent.createTest("removeItem")
+                .log(Status.PASS, "TODO: removeItem");
+        extent.flush();
     }
 
     // TODO update quantity
+    /**
+     * Test to validate updating an item quantity to the inventoryList
+     * @author: jimgray9999
+     */
     @Test
     public void updateQuantity() {
         // add the number to the quantity
         // will either deduct if a negative number or add if positive
-        System.out.println("Updated quantity of item.");
+        extent.createTest("updateQuantity")
+                .log(Status.PASS, "TODO: updateQuantity");
+        extent.flush();
     }
 
+    /**
+     * Helper method for printing out all inventory items
+     *
+     * @param arrListToPrint - the arrayList that should contain all the Inventory class items
+     */
     public void printAllItems(ArrayList<Inventory> arrListToPrint){
         for (Inventory inventory : arrListToPrint) {
             System.out.println(inventory.toString());
